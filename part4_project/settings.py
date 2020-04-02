74f8beaa1ef4459ecd5cd6145252791b7e492678
Server <server.ares@gmail.com>
2020-04-03 00:51:40 +0300

    v0.0.2.4 - fix static

--- part4_project/settings.py
+++ part4_project/settings.py
@@ -148,5 +148,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'image')
 MEDIA_URL = '/image/'
 
+STATICFILES_FINDERS = (
+'django.contrib.staticfiles.finders.FileSystemFinder',
+'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
--- part4_project/urls.py
+++ part4_project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('main.urls')),
@@ -31,4 +32,6 @@ urlpatterns = [
     path('brand/', include('brand.urls')),
     path('filter/', include('filter.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-] #+ static(settings.STATIC_URL, document_root=settings.STATIC_URL) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#+ static(settings.STATIC_URL, document_root=settings.STATIC_URL) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
