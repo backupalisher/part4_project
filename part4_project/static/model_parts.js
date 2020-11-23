2000b30eae74af8b39f884727c1a3005bc997c85
ares <server.ares@gmail.com>
2020-11-23 18:42:47 +0300

    v0.1.1.2d add filter reaction in parts

--- part4_project/static/model_parts.js
+++ part4_project/static/model_parts.js
@@ -4,7 +4,17 @@ $(document).ready(function () {
             const elem = $('a:contains('+ $(this).val().toUpperCase() +')' );
             if (elem.length > 0) {
                 $id = elem.closest('.model-opts').first().attr('id');
+                $('#'+$id+' .part-item').removeClass('active');
                 document.getElementsByClassName($id)[0].click();
+                $items = document.querySelectorAll('#'+$id+' .part-item')
+                $items.forEach(x => {
+                    if (x.innerHTML.indexOf($(this).val().toUpperCase()) > -1) {
+                        x.classList.add('active')
+                        $('#'+$id).parent().parent().animate({
+                            scrollTop: x.offsetTop
+                        }, 200);
+                    }
+                });
             }
         } else {
 
