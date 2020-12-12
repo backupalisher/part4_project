7d1b8ff3383bc1b0310ed2ee58f134a2a4a169e5
ares <server.ares@gmail.com>
2020-12-12 12:37:14 +0300

    v0.1.1.5a remove pip phonenumbers

--- part4_project/apps/main/templatetags/main_extras.py
+++ part4_project/apps/main/templatetags/main_extras.py
@@ -1,6 +1,5 @@
 from django import template
 import locale
-import phonenumbers
 
 register = template.Library()
 locale.setlocale(locale.LC_ALL, 'ru_Ru')
@@ -14,10 +13,3 @@ def cut(value):
     except:
         return value
 
-
-@register.filter(name='c_phone')
-def fotmat(value):
-    try:
-        return phonenumbers.parse(value, "RU")
-    except:
-        return 'invalid'
