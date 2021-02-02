4077656f2902a9f4844db31570a387364065e34a
ares <server.ares@gmail.com>
2021-02-02 13:24:51 +0300

    v0.1.2.1g fix js

--- part4_project/static/model_parts.js
+++ part4_project/static/model_parts.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
     $('#parts_search').keyup(function () {
         if ($(this).val().length > 1) {
-            const elem = $('a:contains('+ $(this).val().toUpperCase() +')' );
+            const elem = $('.part-item_desc:contains('+ $(this).val().toUpperCase() +')' );
             if (elem.length > 0) {
                 $id = elem.closest('.model-opts').first().attr('id');
                 $('#'+$id+' .part-item').removeClass('active');
                 document.getElementsByClassName($id)[0].click();
                 $items = document.querySelectorAll('#'+$id+' .part-item')
+                console.log($items)
                 $items.forEach(x => {
                     if (x.innerHTML.indexOf($(this).val().toUpperCase()) > -1) {
                         x.classList.add('active')
