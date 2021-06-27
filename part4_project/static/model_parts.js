1147878cb9a66e191433e1b8b1e62be16ecbdd89
server.ares <server.ares@gmail.com>
2021-06-27 19:28:56 +0300

    v3.2.0 fix styles and bugs

--- part4_project/static/model_parts.js
+++ part4_project/static/model_parts.js
@@ -1,23 +1,25 @@
 $(document).ready(function () {
     $('#parts_search').keyup(function () {
         if ($(this).val().length > 1) {
-            const elem = $('.part-item_desc:contains('+ $(this).val().toUpperCase() +')' );
-            if (elem.length > 0) {
-                $id = elem.closest('.model-opts').first().attr('id');
+            $('.module-title').removeClass('selected-module');
+            const elems = $('.part-item_desc:contains('+ $(this).val().toUpperCase() +')' );
+            if (elems.length > 0) {
+                $id = elems.closest('.model-opts').first().attr('id');
                 $('#'+$id+' .part-item').removeClass('active');
                 document.getElementsByClassName($id)[0].click();
                 $items = document.querySelectorAll('#'+$id+' .part-item')
                 $items.forEach(x => {
                     if (x.innerHTML.indexOf($(this).val().toUpperCase()) > -1) {
                         x.classList.add('active')
-                        $('#'+$id).parent().parent().animate({
-                            scrollTop: x.offsetTop
-                        }, 200);
+//                        $('#'+$id).parent().parent().animate({
+//                            scrollTop: x.offsetTop
+//                        }, 200);
                     }
                 });
             }
         } else {
-
+            $('.part-item').removeClass('active');
+            $('.part-item').addClass('active');
         }
     })
 })
